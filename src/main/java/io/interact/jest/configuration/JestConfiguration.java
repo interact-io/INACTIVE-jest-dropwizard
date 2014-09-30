8751738309667407d13b673c98a71e69ebb7f20a
swampie <matteo.fiandesio@gmail.com>
2014-09-30 16:09:18 +0200

    Jest Configuration

--- src/main/java/io/interact/jest/configuration/JestConfiguration.java
+++ src/main/java/io/interact/jest/configuration/JestConfiguration.java
@@ -22,6 +22,6 @@ public class JestConfiguration {
 
 	@ValidationMethod
     public boolean isValidConfig() {
-        return !StringUtils.isNotEmpty(connectionURL);
+        return !StringUtils.isEmpty(connectionURL);
     }
 }
