7c342717e704b779f7cc693b57872e6e5f7c5a57
swampie <matteo.fiandesio@gmail.com>
2015-07-08 10:17:12 +0200

    sonar issues

--- src/main/java/io/interact/jest/client/GsonAdapter.java
+++ src/main/java/io/interact/jest/client/GsonAdapter.java
@@ -3,7 +3,8 @@ package io.interact.jest.client;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonSerializer;
 
-public abstract class GsonAdapter<T> implements JsonSerializer<T>,JsonDeserializer<T>{
-	public abstract Class<T> getType();
-	
+public interface GsonAdapter<T> extends JsonSerializer<T>, JsonDeserializer<T> {
+
+    public Class<T> getType();
+
 }
--- src/main/java/io/interact/jest/client/ManagedJestClient.java
+++ src/main/java/io/interact/jest/client/ManagedJestClient.java
@@ -17,45 +17,52 @@ import com.google.gson.GsonBuilder;
 
 public class ManagedJestClient implements Managed {
 
-    private JestClient jestClient;
+    private static final int READ_TIMEOUT_MS = 60000;
+
+    private static final int MAX_CONNECTION_IDLE_SECS = 10;
+
+    private final JestClient jestClient;
+
     private final static Logger LOG = LoggerFactory.getLogger(ManagedJestClient.class);
 
     public JestClient getClient() {
-        return jestClient;
+        return this.jestClient;
     }
 
     public ManagedJestClient(final JestConfiguration config) {
         checkNotNull(config, "JestConfiguration must not be null");
 
-        GsonBuilder gb = new GsonBuilder();
+        GsonBuilder gsonBuilder = new GsonBuilder();
         for (String adapter : config.getAdapters()) {
             try {
                 GsonAdapter<?> ga = (GsonAdapter<?>) Class.forName(adapter).newInstance();
-                gb.registerTypeAdapter(ga.getType(), ga);
+                gsonBuilder.registerTypeAdapter(ga.getType(), ga);
             } catch (Exception e) {
                 LOG.error(String.format("Unable to instance class adapter %s", adapter), e);
                 throw new RuntimeException(e);
             }
         }
 
-        Converters.registerDateTime(gb);
+        Converters.registerDateTime(gsonBuilder);
 
         JestClientFactory factory = new JestClientFactory();
 
+        int CONNECTION_TIMEOUT_MS = 30000;
         factory.setHttpClientConfig(new HttpClientConfig.Builder(config.getConnectionURLS()).multiThreaded(true)
 
-        .readTimeout(60000).maxConnectionIdleTime(10, TimeUnit.SECONDS).connTimeout(30000).gson(gb.create()).build());
+        .readTimeout(READ_TIMEOUT_MS).maxConnectionIdleTime(MAX_CONNECTION_IDLE_SECS, TimeUnit.SECONDS)
+                .connTimeout(CONNECTION_TIMEOUT_MS).gson(gsonBuilder.create()).build());
         this.jestClient = factory.getObject();
     }
 
     @Override
     public void start() throws Exception {
-
+        LOG.debug("starting the managed jest client");
     }
 
     @Override
     public void stop() throws Exception {
-        jestClient.shutdownClient();
+        this.jestClient.shutdownClient();
     }
 
 }
