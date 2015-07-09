73ae0a129d87420a52a39b3f162f1c5de6451e49
swampie <matteo.fiandesio@gmail.com>
2015-07-09 09:39:43 +0200

    sonar

--- src/main/java/io/interact/jest/client/GsonAdapter.java
+++ src/main/java/io/interact/jest/client/GsonAdapter.java
@@ -8,19 +8,21 @@ import com.google.gson.JsonSerializer;
  * The GsonAdapter interface wraps the functionality of a
  * serialization/deserialization process.
  * 
+ * <p>
  * This is due to the fact that Jest library uses GSON while almost all interact
  * projects are using Jackson. With this functionality is possible to register
  * an adapter implementation that allows to hook into the gson ser/deser process
  * and replace with Jackson
+ * </p>
  * 
  * @author matteo
  *
  * @param <T>
+ *            the class generics
  */
 public interface GsonAdapter<T> extends JsonSerializer<T>, JsonDeserializer<T> {
 
     /**
-     * 
      * The getType method is used to extract the className of the generics of
      * the implementation of this interface to kickstart the deserialization
      * process of Jackson.
